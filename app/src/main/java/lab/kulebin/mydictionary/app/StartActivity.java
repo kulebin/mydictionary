9c1674437595ba471c77ea6238c5257b588c6f08
Mikhail_Sianko <sianko.mg@gmail.com>
2017-02-04 00:41:38 +0300

    TODO: deleted firebaseUser.reload method; TODO: killing rennable in case user closed app during splash screen showing

--- app/src/main/java/lab/kulebin/mydictionary/app/StartActivity.java
+++ app/src/main/java/lab/kulebin/mydictionary/app/StartActivity.java
@@ -13,7 +13,10 @@ import lab.kulebin.mydictionary.R;
 //TODO show progress indication when any background operation is performed (0,1 s and less - we don't need progress)
 public class StartActivity extends AppCompatActivity {
 
-    private static final int SPLASH_SHOW_TIME = 1000;
+    private static final int SPLASH_SHOW_TIME = 500;
+
+    private Runnable mRunnable;
+    private Handler mHandler;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
@@ -23,8 +26,7 @@ public class StartActivity extends AppCompatActivity {
         final FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
         final FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
 
-        //TODO what if I close app when splash is shown?
-        new Handler().postDelayed(new Runnable() {
+        mRunnable = new Runnable() {
 
             @Override
             public void run() {
@@ -32,13 +34,25 @@ public class StartActivity extends AppCompatActivity {
                 if (firebaseUser == null) {
                     intent = new Intent(StartActivity.this, SignInActivity.class);
                 } else {
-                    //TODO will it restore token if expired?
-                    firebaseUser.reload();
                     intent = new Intent(StartActivity.this, MainActivity.class);
                 }
                 startActivity(intent);
                 finish();
             }
-        }, SPLASH_SHOW_TIME);
+        };
+
+        mHandler = new Handler();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mHandler.postDelayed(mRunnable, SPLASH_SHOW_TIME);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mHandler.removeCallbacks(mRunnable);
     }
 }
